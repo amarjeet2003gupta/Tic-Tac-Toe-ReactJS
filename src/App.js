59c8eb3ff33f6b65564bf02ac0b8c854962ff539
Amarjeet Gupta <amarjeet2003gupta@gmail.com>
2024-01-09 23:25:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './component/tictactoe/tictactoe.jsx';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>  
+  );
+}
+
+export default App;
